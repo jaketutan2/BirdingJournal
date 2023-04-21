016f909713d5537610ef0a28fc70bc6b7b1dd959
jaketutan <jrt15971@comcast.net>
2023-04-21 14:45:48 -0400

    .NET default code

---
+++ BirdingJournal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BirdingJournal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
